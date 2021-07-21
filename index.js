0497b326cfc8d84c7d8c386c779c2c1a428d7ec8
Peter Bacenet <peterbacenet@icloud.com>
2021-07-21 11:06:25 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(number){}
+function subtract(number){}
+function multiply(number){}
+function divide(number){}
+function add(a, b){
+    return a + b ;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n + 1;
+}
+function decrement(n) {
+    return n - 1;
+}
+function makeInt(n) {
+    parseInt (n , 10);
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    parseFloat (n, 10)
+    return parseFloat (n, 10)
+}
